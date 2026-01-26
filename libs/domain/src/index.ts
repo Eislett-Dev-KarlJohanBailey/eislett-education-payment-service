4daf33bf85cd83f032564c66d37f53c895f950b5
Karl-Maker <karljohanbailey98@gmail.com>
2026-01-26 17:49:29 -0400

    fix: add exports for service

--- libs/domain/src/index.ts
+++ libs/domain/src/index.ts
@@ -1,19 +1,37 @@
+// Pricing
+
 export * from "./pricing/app/usecases/create.price.usecase";
 export * from "./pricing/app/usecases/get.price.usecase";
 export * from "./pricing/app/usecases/update.price.usecase";
 export * from "./pricing/app/usecases/delete.price.usecase";
 export * from "./pricing/app/usecases/list.prices.by.product.usecase";
 
-// entities
 export * from "./pricing/domain/entities/price.entity";
 export * from "./pricing/domain/errors/domain.error";
 
-// value objects
 export * from "./pricing/domain/value-objects/billing-type.vo";
 export * from "./pricing/domain/value-objects/interval.vo";
 
-// ports
 export * from "./pricing/app/ports/price.repository.port";
 export * from "./pricing/dynamodb/price.mapper";
 export * from "./pricing/dynamodb/price.repository";
 export * from "./pricing/app/mappers/price.mapper";
+
+// Utils JWT
+
+export * from "./utils/auth/jwt.utils";
+export * from "./utils/auth/jwt.types";
+export * from "./utils/auth/errors/authentication.error";
+
+// Entitlements
+
+export * from "./entitlements/domain/entities/entitlement.entity";
+export * from "./entitlements/domain/value-objects/entitlement-key.vo";
+export * from "./entitlements/domain/value-objects/entitlement-role.vo";
+export * from "./entitlements/domain/value-objects/entitlement-status.vo";
+export * from "./entitlements/domain/entities/entitlement-usage.entity";
+export * from "./entitlements/domain/registry/entitlement.registry";
+export * from "./entitlements/app/ports/entitlement.repository";
+export * from "./entitlements/app/usecases/create.entitlement.usecase";
+export * from "./entitlements/app/usecases/update.entitlement.usecase";
+export * from "./entitlements/app/usecases/get.user.entitlements.usecase";
