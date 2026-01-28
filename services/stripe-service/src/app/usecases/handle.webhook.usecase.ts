fd6331bd7943971ce04e4903b054b4c5e318e916
Karl-Maker <karljohanbailey98@gmail.com>
2026-01-27 21:55:02 -0400

    fix: Property 'getProductUseCase' is declared but its value is never read.

--- services/stripe-service/src/app/usecases/handle.webhook.usecase.ts
+++ services/stripe-service/src/app/usecases/handle.webhook.usecase.ts
@@ -5,7 +5,7 @@ import { StripeCustomerRepository } from "../../infrastructure/stripe-customer.r
 import { StripeClient } from "../../infrastructure/stripe.client";
 import {
   BillingEvent,
-  GetProductUseCase,
+  // GetProductUseCase,
 } from "@libs/domain";
 
 export class HandleWebhookUseCase {
@@ -14,7 +14,7 @@ export class HandleWebhookUseCase {
     private readonly eventPublisher: BillingEventPublisher,
     private readonly customerRepo: StripeCustomerRepository,
     private readonly stripeClient: StripeClient,
-    private readonly getProductUseCase: GetProductUseCase
+    // private readonly getProductUseCase: GetProductUseCase
   ) {}
 
   async execute(event: Stripe.Event): Promise<void> {
